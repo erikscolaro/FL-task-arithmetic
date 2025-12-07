fix: make dino require gradient updates

models/dino_icarl.py
@@ -9,13 +9,13 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class DinoIcarlModel(nn.Module):
     def __init__(self, device=DEVICE):
         super().__init__()
-        self.dino_backbone = Dino
+        self.dino_backbone = Dino()
         self.icarl_head = get_trained_icarl_classifier(device=device)
+        for param in self.icarl_head.parameters():
+            param.requires_grad = False
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         features = self.dino_backbone(x)
-        # No gradients for pretrained icarl head
-        with torch.no_grad():
-            out = self.icarl_head(features)
+        out = self.icarl_head(features)
         
         return out

models/icarl_head.py
@@ -151,19 +151,17 @@ class Icarl(nn.Module):
 
 
 
-run = wandb.init(
-    entity=ENTITY,
-    project=PROJECT,
-    group=GROUP,
-    name="iCaRL_CIFAR100",
-    id=RUN_ID,
-    resume="allow",
-    mode="online",
-)
-
 def get_trained_icarl_classifier(device=DEVICE) -> nn.Module | None:
     try:
-
+        run = wandb.init(
+            entity=ENTITY,
+            project=PROJECT,
+            group=GROUP,
+            name="iCaRL_CIFAR100",
+            id=RUN_ID,
+            resume="allow",
+            mode="online",
+        )
         icarl = Icarl(
             num_classes=100,
             memory_size=TOTAL_EXEMPLARS_VECTORS,